be9d8d56fef70c3ed9960257bb01cd7485e113e7
Matheus Francisco <matheusfrancisco1103@gmail.com>
2024-05-13 02:48:47 -0300

    teste 4 completo

--- trabalhos/Trabalho T/fs.cpp
+++ trabalhos/Trabalho T/fs.cpp
@@ -5,6 +5,52 @@
 #include "fs.h"
 #include <fstream>
 #include <math.h>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <cstring> // Include the <cstring> header to access the strcmp function
+
+// std::vector<std::string> splitString(const std::string& input, char delimiter) {
+//     std::istringstream iss(input);
+//     std::string token;
+//     std::vector<std::string> parts;
+
+//     // Dividindo a string pelo delimitador
+//     while (std::getline(iss, token, delimiter)) {
+//         if (!token.empty()) {  // Ignorar tokens vazios
+//             parts.push_back(token);
+//         }
+//     }
+
+//     return parts;
+// }
+
+std::string getPenultimateDirectory(const std::string& path) {
+    // Encontrar a última barra
+    size_t lastSlash = path.rfind('/');
+    if (lastSlash == std::string::npos) {
+        // Não há barras no caminho, retorno inválido
+        return "Invalid path";
+    }
+
+    // Caso especial para quando há apenas um diretório
+    if (lastSlash == 0) {
+        return "/";
+    }
+
+    // Encontrar a penúltima barra
+    size_t penultimateSlash = path.rfind('/', lastSlash - 1);
+    if (penultimateSlash == std::string::npos) {
+        // Somente um diretório no caminho
+        return "/";
+    }
+
+    // Retornar o penúltimo diretório
+    return path.substr(penultimateSlash + 1, lastSlash - penultimateSlash - 1);
+}
+
 int findFirstFreeBlock(unsigned int value) {
     int bitCount = sizeof(value) * 8;  // Total de bits no tipo de 'value' (geralmente 32 bits para um unsigned int)
 
@@ -16,6 +62,22 @@ int findFirstFreeBlock(unsigned int value) {
 
     return -1;
 }
+
+unsigned int calculateNewBitMap(unsigned int value, int quantityBlocksUsed) {
+    int bitCount = sizeof(value) * 8;  // Total de bits no tipo de 'value' (geralmente 32 bits para um unsigned int)
+
+    for (int i = 0; i < bitCount; i++) {
+        if (!(value & (1 << i))) {  // Verifica se o i-ésimo bit é 0
+            for(int j = i; j < quantityBlocksUsed + i; j++){
+                value |= (1 << j);  // Configura o i-ésimo bit como 1
+            }
+            return value;  // Retorna o valor atualizado
+        }
+    }
+
+    return value;  // Retorna o valor original se todos os bits já estiverem configurados como 1
+}
+
 void initFs(std::string fsFileName, int blockSize, int numBlocks, int numInodes){
     
     // Inicializando o arquivo para leitura
@@ -68,6 +130,7 @@ void addFile(std::string fsFileName, std::string filePath, std::string fileConte
     int blockSize = 0x00;
     int numBlocks = 0x00;
     int numInodes = 0x00;
+    int sizePai = 0x00;
 
     file.read((char*) &blockSize, sizeof(char));
     file.read((char*) &numBlocks, sizeof(char));
@@ -75,12 +138,31 @@ void addFile(std::string fsFileName, std::string filePath, std::string fileConte
 
     int mapBitSize = std::ceil(numBlocks/8.0);
     char *mapBits = new char[mapBitSize]{};
-
+    char diretorioPai[10]{};
 
     file.read(mapBits, mapBitSize * sizeof(char));
-    
-    int indiceNovoInode{0};
 
+    std::string dir = getPenultimateDirectory(filePath);
+    int indicePai = 0;
+    strcpy(diretorioPai, dir.c_str());
+
+
+    for (; indicePai < numInodes; indicePai++){
+        if(file.read((char*) &inodes, sizeof(INODE))){
+            int result = std::strcmp(diretorioPai, inodes.NAME);
+            if( result == 0){
+                inodes.SIZE += 0x01;
+                file.seekg(16 + indicePai * sizeof(INODE));
+                file.write((char*) &inodes.SIZE, sizeof(char));
+                break;
+            }
+        }
+    }
+
+
+
+    int indiceNovoInode{0};
+    file.seekg(4);
     for (; indiceNovoInode < numInodes; indiceNovoInode++){
         if(file.read((char*) &inodes, sizeof(INODE))){
             if(inodes.IS_USED == 0x00){
@@ -88,7 +170,27 @@ void addFile(std::string fsFileName, std::string filePath, std::string fileConte
             }
         }
     }
+        file.seekg(4 + indicePai * sizeof(INODE));
+    file.read((char*) &inodes, sizeof(INODE));
     
+    for (int i(0); i < (sizeof(inodes.DIRECT_BLOCKS) / sizeof(inodes.DIRECT_BLOCKS[0])); i++){
+        bool isFind = false;
+        file.seekg(5 + numInodes * sizeof(INODE) + inodes.DIRECT_BLOCKS[i] * blockSize);
+        for (int j(0); j < blockSize; j++){
+            char aux;
+            if (file.read((char*) &aux, sizeof(char))){
+                if(aux == 0x00){
+                    file.seekg(5 + numInodes * sizeof(INODE) + inodes.DIRECT_BLOCKS[i] * blockSize + j);
+                    file.write((char*) &indiceNovoInode, sizeof(char));
+                    isFind = true;
+                    break;
+                }
+            }
+        }
+        if (isFind){
+            break;
+        }
+    }
     // blocos 0 = filhos de raiz txt | agora é encontrar 1 e 2 = conteudo do txt
     int quantityBlocks = std::ceil(static_cast<double>(fileContent.size())/blockSize);
     int freeBlock = findFirstFreeBlock(*mapBits);
@@ -120,38 +222,16 @@ void addFile(std::string fsFileName, std::string filePath, std::string fileConte
     //     inodes.DIRECT_BLOCKS[i] = fileContent[i];
     // }
 
-
+    //incrementar size do pai
     file.seekg(4 + indiceNovoInode * sizeof(INODE));
     file.write((char*) &inodes, sizeof(INODE));
     
+
     //atualiza o mapa de bits
-    file.seekg(5 + numInodes * sizeof(INODE));
-    // file.write((char*)0x01, sizeof(char));
-    for (int i(0); i< numBlocks; i++){
-        bool flag = false;
-        for (int j(0); j< blockSize; j++){
-            char aux;
-            if (file.read((char*) &aux, sizeof(char))){
-                if(aux != 0x00){
-                    flag = true;
-                    break;
-                }
-            }
-            
-            // if (file.read((char*) &mapBits[i], sizeof(char))){
-            
-            // }
-        }
-        
-        mapBits[i] = flag ? 0x01 : 0x00 ;
-        
-    }
+    file.seekg(3);
+    *mapBits = calculateNewBitMap(*mapBits, quantityBlocks);
+   
     file.write(mapBits, mapBitSize * sizeof(char));
-    
-    file.seekg(4 + sizeof(INODE));
-
-
-
 
 
     file.close();
